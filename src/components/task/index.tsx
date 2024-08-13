d4174c70512f3d15bf743c4bfe61d8fdf8d927e9
Kota Ishida <kota.ishida@di-v.co.jp>
2024-08-14 01:47:50 +0900

    first-commit

---
+++ src/components/task/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Select } from "../select";
+import "./style.css";
+
+export const Task = () => {
+  return (
+    <div className="task" style={{ backgroundColor: "white" }}>
+      【ここを編集】
+    </div>
+  );
+};
